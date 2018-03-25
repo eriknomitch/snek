1585900ac3416e9ec80ca54fd661fc9547611af3
Erik Nomitch <enomitch@gmail.com>
2018-03-25 10:48:07 -0500

    Add second arm

--- examples/custom-alt.lisp
+++ examples/custom-alt.lisp
@@ -38,6 +38,30 @@
           (snek:with (snk :zwidth rad)
             (snek:append-edge?
               curr (vec:add (snek:get-vert snk curr) (rnd:in-circ rad))
+              :rel nil)))))
+
+    (labels ((new-append-edge-alt* (snk a)
+               (if (<= (length (snek:verts-in-rad snk (snek::append-edge-alt-xy a) rad)) 1)
+                 (aif (snek::do-append-edge-alt snk a)
+                   (progn
+                     (incf i)
+                     (setf curr it)
+                     (sandpaint:set-fg-color sand (color:rgb 1.0 1.0 1.0 0.01))
+                     (sandpaint:circ sand (list (snek::append-edge-alt-xy a)) 4d0 3000)
+                     (sandpaint:set-fg-color sand (color:white 0.01))
+                     (circ-stroke sand (snek:get-verts snk (list it (snek::append-edge-alt-v a))))
+                     (sandpaint:save sand (format nil "exports/~a-~3,'0d" fn i)))))))
+
+      (let ((snk (snek:make
+                   :max-verts n
+                   :alts `((snek::append-edge-alt ,#'new-append-edge-alt*)))))
+
+        (setf curr (snek:add-vert! snk mid))
+
+        (loop for i from 0 below n do
+          (snek:with (snk :zwidth rad)
+            (snek:append-edge?
+              curr (vec:add (snek:get-vert snk curr) (rnd:in-circ rad))
               :rel nil)))))))
 
 
